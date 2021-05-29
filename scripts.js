db45a2b64afa31a14877dc163cbe5b321da1dd99
SijiaMao <sijia501@gmail.com>
2021-05-28 23:21:15 -0400

    created some arrays :scroll: :scroll: :scroll:

---
+++ scripts.js
@@ -0,0 +1,11 @@
+chequeApp = {};
+
+chequeApp.init = () => {
+    chequeApp.singleDigits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    chequeApp.teenDigits = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    chequeApp.tyDigits = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    chequeApp.bigDigits = ['hundred', 'thousand', 'million', 'billion'];
+};
+
+
+chequeApp.init();
